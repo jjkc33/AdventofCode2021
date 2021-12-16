4e0b28398d19caa7eb9f250ba912b4908a2c299d
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-12-15 22:13:22 -0800

    day 10 part 2

--- Day10/solution.py
+++ Day10/solution.py
@@ -6,10 +6,12 @@ SCORING = {
     '>': 25137,
 }
 
-# part 1
+
 openers = ['(', '[', '{', '<']
 closers = [')', ']', '}', '>']
-answer = 0
+
+answer = 0  # part 1 solution
+completion_scores = []  # part 2 scores
 
 with open(r'./input.txt') as f:
     for line in f:
@@ -17,7 +19,23 @@ with open(r'./input.txt') as f:
         for c in line.strip():
             if c in openers:
                 stack.append(c)
-            elif stack.pop() != openers[closers.index(c)]:
+            elif stack.pop() != openers[closers.index(c)]:  # part 1
                 answer += SCORING[c]
+                break
+        else:  # part 2
+            score = 0
+            for c in reversed(stack):
+                score = 5 * score + openers.index(c) + 1
 
+            completion_scores.append(score)
+
+
+# part 1
 print(answer)
+
+
+# part 2
+completion_scores.sort()
+n_incomplete = len(completion_scores)
+answer2 = completion_scores[n_incomplete // 2]
+print(answer2)
