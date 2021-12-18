4a90c5a36607b849b643bdc6951a67950b26e4a0
Jack Casey <25445014+jjkc33@users.noreply.github.com>
2021-12-18 13:37:45 -0800

    day 16 part 2

--- Day16/solution.py
+++ Day16/solution.py
@@ -1,4 +1,5 @@
-
+import operator
+from functools import reduce
 
 def hex2bin(s):
     b = bin(int(s, 16))[2:]
@@ -25,7 +26,7 @@ for c in msg:
     b_msg += hex2bin(c)
 
 
-def parse(s, vsum=0):
+def parse_message(s, vsum=0):
     packet_version = int(s[:3], 2)
     vsum += packet_version
     type_id = int(s[3:6], 2)
@@ -33,26 +34,46 @@ def parse(s, vsum=0):
     s = s[6:]
     if type_id == 4:  # literal values
         val, s = literal_values(s)
-    else:  # operators
-        length_type_id = s[0]
-        if length_type_id == '0':  # next 15 bits
-            length_in_bits = int(s[1:16], 2)
-            s = s[16:]
-            subpackets = s[:length_in_bits]
-            subpacket_sum = 0
-            while subpackets:
-                subpackets, subpacket_sum = parse(subpackets, subpacket_sum)
+        return s, val, vsum
 
-            vsum += subpacket_sum
-            s = s[length_in_bits:]
-        else:  # next 11 bits
-            num_sub_packets = int(s[1:12], 2)
-            s = s[12:]
-            for i in range(num_sub_packets):
-                s, vsum = parse(s, vsum)
+    length_type_id = s[0]
+    if length_type_id == '0':  # next 15 bits
+        length_in_bits = int(s[1:16], 2)
+        s = s[16:]
+        subpackets = s[:length_in_bits]
+        subpacket_vals = []
+        while subpackets:
+            subpackets, subpacket_val, vsum = parse_message(subpackets, vsum)
+            subpacket_vals.append(subpacket_val)
 
-    return s, vsum
+        s = s[length_in_bits:]
+    else:  # next 11 bits
+        num_sub_packets = int(s[1:12], 2)
+        s = s[12:]
+        subpacket_vals = []
+        for i in range(num_sub_packets):
+            s, subpacket_val, vsum = parse_message(s, vsum)
+            subpacket_vals.append(subpacket_val)
+
+    result = 0
+    if type_id == 0:
+        result = sum(subpacket_vals)
+    elif type_id == 1:
+        result = reduce(operator.mul, subpacket_vals)
+    elif type_id == 2:
+        result = min(subpacket_vals)
+    elif type_id == 3:
+        result = max(subpacket_vals)
+    elif type_id == 5:
+        result = int(subpacket_vals[0] > subpacket_vals[1])
+    elif type_id == 6:
+        result = int(subpacket_vals[0] < subpacket_vals[1])
+    elif type_id == 7:
+        result = int(subpacket_vals[0] == subpacket_vals[1])
+
+    return s, result, vsum
 
 
-_, version_sum = parse(b_msg)
-print(version_sum)
+_, answer2, answer1 = parse_message(b_msg)
+print(answer1)
+print(answer2)
